e378e034b55b75afd46e332b29287c8c302acfeb
Patrick Liang <pl204511194@yahoo.com>
2022-02-23 11:14:01 -0500

    commit tree 82549de650a2fc36c40b07a41b2cc191496368ee

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -2,19 +2,33 @@ require 'pry'
 
 class String
 
+  def showing_self
+    self
+  end
+
   def sentence?
-    
+    self.end_with?(".")
   end
 
   def question?
-
+    self.end_with?("?")
   end
 
   def exclamation?
-
+    self.end_with?("!")
   end
 
   def count_sentences
-
+    separated = self.split(/\.|\?|\!/)
+    filtered_strings = separated.filter do |string|
+      string.length > 0
+    end
+    filtered_strings.count
   end
 end
+
+somesentence = String.new
+
+somesentence = "This, well, is a sentence. This is too!! And so is this, I think? Woo..."
+
+print somesentence.count_sentences
